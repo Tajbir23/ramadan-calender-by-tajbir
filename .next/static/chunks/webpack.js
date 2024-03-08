5cedb5a0496e17ef6ba899572e7061d62f5c9ad3
Tajbir islam <studenttajbirislam@gmail.com>
2024-03-08 14:46:24 +0600

    integrated api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "319b58310a8fcf7d"; }
+/******/ 		__webpack_require__.h = function() { return "780d9089fbe181dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
