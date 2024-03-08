96546c1158b26990519d9ace8b5b65a37d4faf34
Tajbir islam <studenttajbirislam@gmail.com>
2024-03-08 08:43:55 +0600

    ramadan project client side

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Slider from "@/components/Slider";
+import Sections from "@/components/Sections";
+import Navber from "@/components/Navber";
+import Catagory from "@/components/Catagory";
+import PrayerTimeTable from "@/components/RamadanSchedule";
+import Footer from "@/components/Footer";
+import Table from "@/components/Tabble";
+import { useEffect } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+
+  
+  return (
+   <div>
+   <Navber/>
+    <Header/>
+    {/* <Slider/> */}
+    <Sections/>
+    <Catagory/>
+    <Table/>
+    <Footer/>
+   </div>
+  );
+}
